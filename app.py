43fc3fe256ed4819cf2183496e37b0eaf8019ea4
GuptaAman08 <2015aman.a@ves.ac.in>
2018-04-04 12:22:49 +1100

    Session implemented , route secured

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template, request, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
 import mongo
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt 
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -16,6 +17,8 @@ def project():
 @app.route('/home', methods=["POST","GET"])
 def landing():
     return render_template('home.html')
+
+
 
 @app.route('/profile', methods=["POST","GET"])
 def profile():
@@ -36,6 +39,7 @@ def faculty():
 
 class LoginForm(Form):
     email = StringField('Email',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Regexp(regex='^[0-9]{4}[a-z]+.[a-z]+@ves.ac.in$',message="Inappropriate Email ID")])
+    name = StringField('Name',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Regexp(regex="[a-zA-Z]+")])
     password = PasswordField('Password',[validators.DataRequired(message="Input is mandatory!!!!"), validators.EqualTo('confirm', message="Passwords do not match")])
     confirm = PasswordField('Confirm Password',[validators.InputRequired(message="Input is mandatory!!!!")])
     contact = StringField('Contact',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Regexp(regex='^[0-9]{10}$',message="Contact number invalid")])
@@ -46,21 +50,81 @@ class LoginForm(Form):
 def register():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
-            email = form.email.data
-            password = sha256_crypt.encrypt(str(form.password.data))
-            contact = form.contact.data
-            div = form.division.data
-            year = form.year.data
+        email = form.email.data
+        name = form.name.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        contact = form.contact.data
+        div = form.division.data
+        year = form.year.data
+        
+        try:
+            result = mongo.studentCollection.insert_one({
+                "email": email,
+                "name": name,
+                "password": password,
+                "contact": contact,
+                "class": div,
+                "year": year
+            })
             flash('Registered successfully!!', 'success')
             return redirect(url_for('login'))
-    return render_template('login.html',form=form)
+        except:
+            flash('insertion error!!', 'danger')
+            return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=["POST","GET"])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password_to_verify = request.form['password']
+        
+        try:
+            print(email)
+            result = mongo.studentCollection.find_one({
+                    "email": email
+                },
+                {
+                    "password": 1,
+                    "name": 1,
+                    "_id": 0
+                }
+            )
+            if sha256_crypt.verify(password_to_verify, result['password']):
+                session['logged_in'] = True
+                session['username'] = result['name']
+                flash('Logged In Successfully', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Incorrect username or password', 'danger')
+                return render_template('login.html')
+        except:
+            flash('Couldnt find in DataBase', 'danger')
+            return render_template('login.html')
     return render_template('login.html')    
+
+#Checked if user is logged in 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if session['logged_in']:
+            return f(*args, **kwargs)
+        else:
+            flash("Unauthorized, Please login", "warning")
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/dashboard', methods=["POST","GET"])
+@is_logged_in
+def dashboard():
+    return render_template('home.html')
+
+@app.route('/logout', methods=["POST","GET"])
+def logout():
+    session['logged_in'] = False
+    flash('You are logged out successfully','success')
+    return redirect(url_for('login'))
+
 
 @app.route('/projects_search', methods=["POST","GET"])
 def projects_search():
--- mongo.py
+++ mongo.py
@@ -3,3 +3,4 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://anyone:anyone@ds113958.mlab.com:13958/project_panorama")
 db = client['project_panorama']
 facultyCollection = db["Faculty"]
+studentCollection = db["Student"]
