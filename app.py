d35dc086a56d21580d2ecb38b7cd6052a8a00701
GuptaAman08 <201aman.a@ves.ac.in>
2018-04-09 00:31:26 +0530

    completing upload project details module

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
 import mongo
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, SelectField, validators
+from wtforms_components import read_only
 from passlib.hash import sha256_crypt 
 from functools import wraps
 
@@ -40,7 +41,7 @@ class LoginForm(Form):
     confirm = PasswordField('Confirm Password',[validators.InputRequired(message="Input is mandatory!!!!")])
     contact = StringField('Contact',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Regexp(regex='^[0-9]{10}$',message="Contact number invalid")])
     division = StringField('Division',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Length(min=3,max=5)])
-    year = StringField('Year_of_joining',[validators.InputRequired(message="Input is mandatory!!!!"),validators.Length(4, 4),validators.Regexp(regex="^[0-9][0-9]{3}$")])
+    year = SelectField("Year of joining", choices=[(year, year) for year in range(2010, 2018)])
 
 @app.route('/register',methods=['POST','GET'])
 def register():
@@ -76,18 +77,20 @@ def login():
         password_to_verify = request.form['password']
         
         try:
-            print(email)
+            
             result = mongo.studentCollection.find_one({
                     "email": email
                 },
                 {
                     "password": 1,
                     "name": 1,
+                    "email": 1,
                     "_id": 0
                 }
             )
             if sha256_crypt.verify(password_to_verify, result['password']):
                 session['logged_in'] = True
+                session['email'] = result['email']
                 session['username'] = result['name']
                 flash('Logged In Successfully', 'success')
                 return redirect(url_for('dashboard'))
@@ -116,20 +119,90 @@ def dashboard():
     return render_template('home.html')
 
 @app.route('/logout', methods=["POST","GET"])
+@is_logged_in
 def logout():
     session['logged_in'] = False
     flash('You are logged out successfully','success')
     return redirect(url_for('login'))
 
 
-@app.route('/search-results', methods=["POST","GET"])
+@app.route('/search-results', methods=['POST','GET'])
 def projects_search():
     return render_template('projects_search.html')    
 
+class UploadProject(Form):
+    proj_leader = StringField('Project Leader registered email ID', [validators.InputRequired(message="Input Mandatory"), validators.Regexp(regex='^[0-9]{4}[a-z]+.[a-z]+@ves.ac.in$',message="Inappropriate Email ID")])
+    proj_member = StringField('Enter registered email ID of other members(should not exceed more than 3 member)',[validators.InputRequired(message="Input Mandatory")], render_kw={"placeholder": "Enter names as comma separated values: eg: xd, xd, xd,..."})
+    proj_name = StringField('Project Name', [validators.InputRequired(message="Input is mandatory!!!!"), validators.Length(min=2, max=40)])
+    project_description = TextAreaField('Project Description', [validators.InputRequired(message="Input is mandatory!!!!") ,validators.Length(min=20, max=180)])
+    category = SelectField(
+        'Category',
+        choices=[('Data Mining', 'Data Mining'), ('AI', 'Artificial Intelligence'), ('Security', 'Security'),
+            ('Networking', 'Networking'), ('IOT', 'IOT'), ('Application Development', 'Application Development'),
+            ('Graphics/Virtual Reality', 'Graphics/Virtual Reality'), ("NLP", "Natural Language Processing")
+        ]
+    )
+    status = SelectField("Status", choices=[('PC', "Partially Completed"), ('C', 'Completed')])
+    semester = SelectField("Semester", choices=[('3', 'Sem 3'), ('4', 'Sem 4'), ('5', 'Sem 5'), ('6', 'Sem 6'), ('7', 'Sem 7')])
+    subject = StringField('Subject Name',[validators.InputRequired(message="Input is mandatory!!!!"), validators.Length(min=2, max=15)], render_kw={"placeholder": "If not part of academics enter non-academic"})
+    academicYear = SelectField("Academic Year", choices=[(str(year), str(year)) for year in range(2010, 2018)])
+    github = StringField('Github Repo',[validators.Regexp(regex='^https://github.com/[a-zA-Z0-9]+/$', message="pattern mismatch!!!")])
+    endurance = TextAreaField('Your Experience', [validators.InputRequired(message="Input is mandatory!!!!") ,validators.Length(min=5, max=180)])
+
 @app.route('/upload_projects', methods=["POST", "GET"])
 @is_logged_in
-def uploadProject():   
-    return render_template('upload.html')
+def uploadProject():
+    form = UploadProject(request.form)
+    if request.method == 'POST' and form.validate():
+        proj_leader = form.proj_leader.data
+        proj_member = form.proj_member.data
+        proj_name = form.proj_name.data
+        description = form.project_description.data
+        category = form.category.data
+        status = form.status.data
+        semester = form.semester.data
+        subject = form.subject.data
+        academicYear = form.academicYear.data
+        github = form.github.data
+        endurance = form.endurance.data
+        member = list(proj_member.split(', '))
+        if len(member) <= 3:
+            try:
+                memberID = []
+                leader = mongo.studentCollection.find_one({
+                    "email": proj_leader
+                },{
+                    "name": 1
+                })
+                for m in member:
+                    result = mongo.studentCollection.find_one(
+                        {"email": m},
+                        {"name": 1}
+                    )
+                    memberID.append(str(result["_id"]))
+                result = mongo.projectCollection.insert_one(
+                    {
+                        "project_name": proj_name,
+                        "project_description": description,
+                        "project_leader": leader["_id"],
+                        "project_members": memberID,
+                        "project_category": category,
+                        "project_status": status,
+                        "project_subject": subject,
+                        "project_academic_year": academicYear,
+                        "project_semester": semester,
+                        "project_github_repo": github,
+                        "project_experiences": endurance,
+                        "project_rating": []
+
+                    }
+                )
+                flash("Project uploaded sucessfully", "success")    
+            except:    
+                flash('Insertion Error Invalid member!!!!', "danger")
+        else:
+            flash("Upload failed #(Members) can't exceed 4", "warning")
+    return render_template('upload.html', form=form)
 
 @app.route('/projects')
 def project():  
--- mongo.py
+++ mongo.py
@@ -4,3 +4,4 @@ client = MongoClient("mongodb://anyone:anyone@ds113958.mlab.com:13958/project_pa
 db = client['project_panorama']
 facultyCollection = db["Faculty"]
 studentCollection = db["Student"]
+projectCollection = db["Project"]
